925a229d2e80ddfdff5a4b8be87dc0b67ae95cf6
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-12 23:52:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   name_keys = []
   organized_data = {}
-  data.map do |key, value|
+  data.each_with_object({}) do |k, v, a|
+    
     
 end
