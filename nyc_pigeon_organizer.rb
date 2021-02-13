b53ee99f09127132c849a860df62035add2c8619
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-13 00:09:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  name_keys = []
-  organized_data = {}
-  data.each_with_object({}) do |k, v, a|
-    
+  data.each_with_object({}) do |(key, value), array|
+    value.each do |key_2, names|
+      names.each do |name|
+        if !array[name]
+          array[name] = {}
+        end
+        if !array[name][key]
+          array[name][key] = []
+        end
+        array[name][key].push(key_2)
+      end
+    end
+    array
   end
     
 end
